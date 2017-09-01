c1b9d093a74a9565a38d239e7b97a8b88cc08b2f
Kosar Hosseinkhani <kosarh79@gmail.com>
2017-09-01 10:44:42 -0700

    Check allowed file number + handle messages

--- client/helper.js
+++ client/helper.js
@@ -1,20 +1,57 @@
-import { Session } from 'meteor/session';
-import { gifMaker } from "meteor/meteor-gifmaker";
+import {Session} from 'meteor/session';
+import {gifMaker} from "meteor/meteor-gifmaker";
 export const helper = {
-    handleFileAdd:(files)=>{
+
+    handleFileAdd: (files) => {
+        let addedFiles = Session.get('files');
+        //number of allowed files to be added
+        let maxAllowedFiles = Session.get('maxAllowedFiles');
+        //add the message either error or info to session
+        let addMessage = (message, messageType) => {
+            let messages = Session.get('messages');
+            message = {
+                message: message,
+                error: messageType==='error',
+                info: messageType==='info',
+            };
+            messages.push(message);
+            Session.set('messages', messages);
+        };
+        let checkFileNumbers = (files)=>{
+            if (addedFiles.length === maxAllowedFiles) {
+                return [];
+            }
+            else{
+                let _files = Array.from(files).slice(0);
+                let capacity = maxAllowedFiles - addedFiles;
+                if(_files.length > capacity){
+                    _files.splice(0, capacity)
+                }
+                return _files;
+            }
+        };
+        
+        //check number of allowed files
+        files = checkFileNumbers(files);
+        if(files.length === 0){
+            addMessage(`Sorry! You can't add more han ${maxAllowedFiles} files!`, 'error');
+            return;
+        }
+        //loop through selected files, read them and add them to Session
         _.each(files, (blob) => {
-            gifMaker.saveFile(blob, (err, src)=>{
-                if(!err){
-                    let files = Session.get('files');
-                    files.push({src});
-                    Session.set('files', files);
-                    if(files.length === 2){
+            gifMaker.saveFile(blob, (err, src) => {
+                if (!err) {
+                    addedFiles.push({src});
+                    Session.set('files', addedFiles);
+                    //check number of allowed files
+                    if (addedFiles.length === maxAllowedFiles) {
+                        addMessage(`Great! you added all allowed files. Please hit submit to receive your gif!`,
+                            'info');
                         Session.set('disableFileSelection', true);
                     }
                 }
-                else{
-                    let error = [`${blob.name} is not an acceptable image! Sorry!`];
-                    Session.set('errors', error);
+                else {
+                    addMessage(`${blob.name} is not an acceptable image! Sorry!`, 'error');
                 }
             })
         });
--- client/main.js
+++ client/main.js
@@ -1,72 +1,75 @@
-import { Template } from 'meteor/templating';
-import { ReactiveVar } from 'meteor/reactive-var';
+import {Template} from 'meteor/templating';
 import {helper} from './helper';
 import './main.html';
-import { Session } from 'meteor/session';
+import {Session} from 'meteor/session';
 
+Session.set('maxAllowedFiles',2);
 Session.set('files', []);
-Session.set('errors', []);
+Session.set('messages', []);
 Session.set('disableFileSelection', false);
 
-Template.gifmaker.onRendered(function() {
-    let holder = document.getElementById('holder'),
-        tests = {
+Template.gifmaker.onRendered(function () {
+    let holder = document.getElementById('holder');
+    let   tests = {
             filereader: typeof FileReader != 'undefined',
             dnd: 'draggable' in document.createElement('span'),
             formdata: !!window.FormData,
             progress: "upload" in new XMLHttpRequest
-        },
-        support = {
+        };
+    let support = {
             filereader: document.getElementById('filereader'),
             formdata: document.getElementById('formdata'),
             progress: document.getElementById('progress')
         };
-
-
-    "filereader formdata progress".split(' ').forEach(function (api) {
+    "filereader formdata progress".split(' ').forEach((api) => {
         if (tests[api] === false) {
             support[api].className = 'fail';
         } else {
-            // FFS. I could have done el.hidden = true, but IE doesn't support
-            // hidden, so I tried to create a polyfill that would extend the
-            // Element.prototype, but then IE10 doesn't even give me access
-            // to the Element object. Brilliant.
             support[api].className = 'hidden';
         }
     });
 
     if (tests.dnd) {
-        holder.ondragover = function () { this.className = 'hover'; return false; };
-        holder.ondragend = function () { this.className = ''; return false; };
+        holder.ondragover = function () {
+            this.className = 'hover';
+            return false;
+        };
+        holder.ondragend = function () {
+            this.className = '';
+            return false;
+        };
         holder.ondrop = function (e) {
             this.className = '';
             e.preventDefault();
-          //  readfiles(e.dataTransfer.files);
             helper.handleFileAdd(e.dataTransfer.files);
         }
     } else {
         fileupload.className = 'hidden';
         fileupload.querySelector('input').onchange = function () {
-           // readfiles(this.files);
+            //fires when user adds files
             helper.handleFileAdd(this.files);
         };
     }
-
 });
 
 Template.gifmaker.events({
-    'change input': (ev) =>{
+    'change input': (ev) => {
         helper.handleFileAdd(ev.target.files);
     }
 });
 
 Template.gifmaker.helpers({
-    disableFileSelection:()=>{
+    disableFileSelection: () => {
         return Session.get('disableFileSelection');
     }
 });
 Template.images.helpers({
-    images:()=>{
+    images: () => {
         return Session.get('files');
+    }
+});
+Template.messages.helpers({
+    messages: () => {
+        return Session.get('messages');
     }
 });
